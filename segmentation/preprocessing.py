a39ffa9efac4331651bcd8e424cda1a8fe87c5e8
HidenYT <buharev2009@gmail.com>
2026-03-08 15:32:23 +0300

    fix seg preprocessing

--- segmentation/preprocessing.py
+++ segmentation/preprocessing.py
@@ -8,9 +8,10 @@ from utils.cv import normalize_image, window_level
 WINDOW_LEVEL = (350, 40)
 
 def preprocess_for_segmentation(slice_img: np.ndarray):
-
-    # slice_img = window_level(slice_img, *WINDOW_LEVEL)
-    slice_img = normalize_image(slice_img)
+    # window, level = WINDOW_LEVEL
+    window, level = 2000, 0
+    slice_img = window_level(slice_img, window=window, level=level)
+    slice_img = normalize_image(slice_img, min=level-window/2, max=level+window/2)
     slice_img = resize(slice_img, (256,256), mode='constant', anti_aliasing=True)
 
     t = torch.from_numpy(slice_img).float()
--- utils/cv.py
+++ utils/cv.py
@@ -7,5 +7,9 @@ def window_level(img: np.ndarray, window: int, level: int):
     return img.clip(low, high)
 
 
-def normalize_image(img):
-    return (img-img.min())/(img.max()-img.min())
+def normalize_image(img, min=None, max=None):
+    if min is None:
+        min = img.min()
+    if max is None:
+        max = img.max()
+    return (img-min)/(max-min)

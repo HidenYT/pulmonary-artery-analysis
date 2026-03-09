d9df860663ed856a19b9ee09540484f667dd34fd
HidenYT <buharev2009@gmail.com>
2026-03-09 11:41:57 +0300

    fix classification preproccessing

--- classification/preprocessing.py
+++ classification/preprocessing.py
@@ -3,19 +3,14 @@ import torch.nn.functional as F
 
 
 def preprocess_for_classifier(slice_img):
-
-    t = torch.from_numpy(slice_img)
-
-    t = t.clamp(-1000, 1000)
-    t = t.float().div(1000.0)
-
+    t = torch.from_numpy(slice_img).float()
     t = t.unsqueeze(0).unsqueeze(0)
-
     t = F.interpolate(
         t,
         size=(128,128),
         mode="bilinear",
-        align_corners=False
+        align_corners=False,
+        antialias=True,
     )
-
+    t.clamp_(-1000, 1000).div_(1000.0)
     return t
